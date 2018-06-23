45482fca1073958fb7b11f99e57ad214662391a7
dvarubla <dvarubla@users.noreply.github.com>
2018-06-23 13:33:46 +0700

    Подготовка окружения

---
+++ src/BinomialCalculator.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace test_task_main {
+    class BinomialCalculator {
+
+    };
+}
---
+++ tests/BinomialCalculatorT.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <BinomialCalculator.h>
+
+//Проверка работы класса-калькулятора
+
+class BinomialCalculatorT: public ::testing::Test {
+public:
+    BinomialCalculatorT() {
+    }
+};
+
+TEST_F(BinomialCalculatorT, Nothing){
+}
